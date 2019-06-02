1805597ffd9eeb9317676f635b28ee39db8454a2
Brahm Lower <bplower@gmail.com>
2019-06-02 16:08:09 -0700

    Help menu for makefiles, runtests.sh cleanup
    Converted lots of paths in the runtests.sh to variables and am
    referencing the repo root instead of relative paths (so much better
    now!).

--- stack_test/runtest.sh
+++ stack_test/runtest.sh
@@ -1,25 +1,36 @@
 #!/usr/bin/env bash
 
-# Clear the test log
-rm -f vtable_server.log
-rm -f docker_db.log
+GIT_ROOT=$(git rev-parse --show-toplevel)
 
-# Start the database and give it a second to spin up
-docker-compose up -d db
-sleep 4
+LOGFILE_SERVER="$GIT_ROOT/stack_test/vtable_server.log"
+LOGFILE_DATABASE="$GIT_ROOT/stack_test/docker_db.log"
+POSTMAN_ENV_FILE="$GIT_ROOT/stack_test/stack_test.postman_environment.json"
+POSTMAN_COLLECTION_FILE="$GIT_ROOT/vtable_server/vtable_server.postman.json"
+SERVER_SETTINGS="$GIT_ROOT/stack_test/settings.yml"
+
+# Clear the test log
+rm -f $LOGFILE_SERVER
+rm -f $LOGFILE_DATABASE
+
+# Start the database and wait for it to start accepting connections
+docker-compose up --detach --force-recreate
+
+until pg_isready --quiet -p 2345 -h localhost; do
+    sleep .1
+done
 
 # Schema and function setup
-PGPASSWORD=test psql -U test -h localhost -p 2345 test -c "\i ../deploy.sql"
+PGPASSWORD=test psql -U test -h localhost -p 2345 test -c "\i $GIT_ROOT/deploy.sql"
 
 # Run the server
-# cd ../vtable_server && GUNICORN_CMD_ARGS="--access-logfile '-' --log-level DEBUG" gunicorn --bind 0.0.0.0:8765 'vtable_server:build_app("../stack_test/settings.yml")' &> ../stack_test/vtable_server.log &
-cd ../vtable_server && GUNICORN_CMD_ARGS="--access-logfile '-' --log-level DEBUG" gunicorn --bind 0.0.0.0:8765 'src:build_app("../stack_test/settings.yml")' &> ../stack_test/vtable_server.log &
+# cd $GIT_ROOT/vtable_server && GUNICORN_CMD_ARGS="--access-logfile '-' --log-level DEBUG" gunicorn --bind 0.0.0.0:8765 "vtable_server:build_app(\"$SERVER_SETTINGS\")" &> $LOGFILE_SERVER &
+cd $GIT_ROOT/vtable_server && GUNICORN_CMD_ARGS="--access-logfile '-' --log-level DEBUG" gunicorn --bind 0.0.0.0:8765 "src:build_app(\"$SERVER_SETTINGS\")" &> $LOGFILE_SERVER &
 
 # Start the tests
-newman run --environment ./stack_test.postman_environment.json --timeout-request 60000 ../vtable_server/vtable_server.postman.json
+newman run --environment $POSTMAN_ENV_FILE $POSTMAN_COLLECTION_FILE
 
 # Capture docker logs
-docker-compose logs --no-color > docker_db.log
+docker-compose logs --no-color > $LOGFILE_DATABASE
 
 # Stop the servers
 pkill gunicorn
